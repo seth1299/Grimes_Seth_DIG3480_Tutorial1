f71afa98eef206437433f4920c00d6d3ff9cca46
seth1299 <seth.grimes1999@gmail.com>
2020-01-26 10:07:01 -0500

    Uploading first batch of files
    Don't have time to search for these few "fatal" files so I'm just going to upload a few chunks at a time until I find them.

---
+++ Grimes_Seth_DIG3480_Project1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical" );
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+            if ( count >= 12 )
+            {
+                winText.text = "You win! Game created by Seth Grimes!";
+            }
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+
+
+}
